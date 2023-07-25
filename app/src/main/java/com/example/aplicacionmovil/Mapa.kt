9019978c18c7f21e88289476d3f38d9ce4f4f938
InoValz <adolfo.inostroza99@gmail.com>
2023-07-25 00:41:42 -0400

    version 2.2.2

--- app/src/main/java/com/example/aplicacionmovil/Mapa.kt
+++ app/src/main/java/com/example/aplicacionmovil/Mapa.kt
@@ -9,16 +9,25 @@ import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.navigation.NavigationView
 
-class Mapa : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class Mapa : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener,
+    OnMapReadyCallback {
 
     private lateinit var drawer: DrawerLayout
     private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mapa)
+        createFragment()
 
         val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_mapa2)
         setSupportActionBar(toolbar)
@@ -37,38 +46,61 @@ class Mapa : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListene
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId){
-            R.id.nav_inicio -> {
+            R.id.nav_inicio2 -> {
                 Toast.makeText(this, "Inicio", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, MenuPrincipal::class.java)
                 startActivity(intent)
             }
-            R.id.nav_mapa -> {
+            R.id.nav_mapa2 -> {
                 Toast.makeText(this, "Mapa", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, Mapa::class.java)
                 startActivity(intent)
             }
-            R.id.nav_foro -> Toast.makeText(this, "Foro", Toast.LENGTH_SHORT).show()
+            R.id.nav_foro2 -> Toast.makeText(this, "Foro", Toast.LENGTH_SHORT).show()
         }
 
         drawer.closeDrawer(GravityCompat.START)
         return true
     }
 
-    override fun onPostCreate(savedInstanceState: Bundle?) {
-        super.onPostCreate(savedInstanceState)
-        toggle.syncState()
+    private fun createFragment() {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapa) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
     }
 
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-        toggle.onConfigurationChanged(newConfig)
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        createMarker()
+
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (toggle.onOptionsItemSelected(item)) {
-            return true
+    private fun createMarker() {
+        val coordinates = LatLng(-37.46973, -72.35366)
+        val marker = MarkerOptions().position(coordinates).title("Los Angeles Chile")
+        map.addMarker(marker)
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(coordinates, 18f),
+            4000,
+            null
+        )
+    }
+
+        override fun onPostCreate(savedInstanceState: Bundle?) {
+            super.onPostCreate(savedInstanceState)
+            toggle.syncState()
         }
 
-        return super.onOptionsItemSelected(item)
+        override fun onConfigurationChanged(newConfig: Configuration) {
+            super.onConfigurationChanged(newConfig)
+            toggle.onConfigurationChanged(newConfig)
+        }
+
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            if (toggle.onOptionsItemSelected(item)) {
+                return true
+            }
+
+            return super.onOptionsItemSelected(item)
+        }
     }
-}
