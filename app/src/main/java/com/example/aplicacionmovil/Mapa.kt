10df5bd3d70634ad6118ff668e0577fd4672e87f
InoValz <adolfo.inostroza99@gmail.com>
2023-12-11 22:26:13 -0300

    version 4.3

--- app/src/main/java/com/example/aplicacionmovil/Mapa.kt
+++ app/src/main/java/com/example/aplicacionmovil/Mapa.kt
@@ -1,15 +1,20 @@
 package com.example.aplicacionmovil
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.content.res.Configuration
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -19,12 +24,12 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 
-class Mapa : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener,
-    OnMapReadyCallback {
+class Mapa : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
 
     private lateinit var drawer: DrawerLayout
     private lateinit var toggle: ActionBarDrawerToggle
     private lateinit var map: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +41,13 @@ class Mapa : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListene
 
         drawer = findViewById(R.id.drawer_Layout2)
 
-        toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open2, R.string.navigation_drawer_close2)
+        toggle = ActionBarDrawerToggle(
+            this,
+            drawer,
+            toolbar,
+            R.string.navigation_drawer_open2,
+            R.string.navigation_drawer_close2
+        )
         drawer.addDrawerListener(toggle)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
@@ -44,10 +55,12 @@ class Mapa : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListene
 
         val navigationView: NavigationView = findViewById(R.id.nav_view2)
         navigationView.setNavigationItemSelectedListener(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId){
+        when (item.itemId) {
             R.id.nav_inicio2 -> {
                 Toast.makeText(this, "Inicio", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, MenuPrincipal::class.java)
@@ -55,8 +68,7 @@ class Mapa : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListene
             }
             R.id.nav_mapa2 -> {
                 Toast.makeText(this, "Mapa", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this, Mapa::class.java)
-                startActivity(intent)
+                // No es necesario crear una nueva instancia de Mapa, ya estás en Mapa
             }
             R.id.nav_notificaciones -> {
                 Toast.makeText(this, "Notificaciones", Toast.LENGTH_SHORT).show()
@@ -65,11 +77,11 @@ class Mapa : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListene
                 Toast.makeText(this, "Mis Sectores", Toast.LENGTH_SHORT).show()
             }
             R.id.nav_cerrar_sesion2 -> {
-                signOut() // Llama a la función para cerrar sesión
+                signOut()
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
                 Toast.makeText(this, "Sesión Cerrada", Toast.LENGTH_SHORT).show()
-                finish() // Cierra la actividad actual para evitar que el usuario regrese presionando "Atrás"
+                finish()
             }
         }
 
@@ -80,13 +92,31 @@ class Mapa : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListene
     private fun createFragment() {
         val mapFragment = supportFragmentManager.findFragmentById(R.id.mapa) as SupportMapFragment
         mapFragment.getMapAsync(this)
-
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
         createMarker()
 
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                location?.let {
+                    val currentLatLng = LatLng(location.latitude, location.longitude)
+                    map.addMarker(MarkerOptions().position(currentLatLng).title("Mi Ubicación"))
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
+                }
+            }
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+            )
+        }
     }
 
     private fun createMarker() {
@@ -99,29 +129,51 @@ class Mapa : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListene
             null
         )
     }
-        override fun onPostCreate(savedInstanceState: Bundle?) {
-            super.onPostCreate(savedInstanceState)
-            toggle.syncState()
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        toggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        toggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
         }
 
-        override fun onConfigurationChanged(newConfig: Configuration) {
-            super.onConfigurationChanged(newConfig)
-            toggle.onConfigurationChanged(newConfig)
-        }
-
-        override fun onOptionsItemSelected(item: MenuItem): Boolean {
-            if (toggle.onOptionsItemSelected(item)) {
-                return true
-            }
-
-            return super.onOptionsItemSelected(item)
-        }
+        return super.onOptionsItemSelected(item)
+    }
 
     private fun signOut() {
         FirebaseAuth.getInstance().signOut()
-        // Borra el ID del usuario de las preferencias compartidas
         val sharedPreferences = getSharedPreferences("Preferencias", Context.MODE_PRIVATE)
         sharedPreferences.edit().remove("UserId").apply()
     }
 
+    companion object {
+        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults) // Agrega esta línea
+
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    onMapReady(map)
+                } else {
+                    Toast.makeText(this, "Permiso de ubicación denegado", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+        }
+    }
+}
